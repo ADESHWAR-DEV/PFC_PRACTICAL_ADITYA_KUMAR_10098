0316d4d6336e932a36d5b83669c250c737561ca3
ADESHWAR-DEV <vikramaditya1125@gmail.com>
2025-12-26 00:36:20 +0530

    Add files via upload

---
+++ 10098 PFC PRACTICAL/6_evenno.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() { 
+    // ADITYA 10098        
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 != 0) {
+            continue;  
+        }
+        sum += i;
+    }
+
+    printf("Sum of even numbers from 1 to %d = %d\n", n, sum);
+
+    return 0;
+}
+/*
+Enter a number: 5
+Sum of even numbers from 1 to 5 = 6
+*/
---
+++ 10098 PFC PRACTICAL/8_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// ADITYA 10098
+void printTriangle(int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int n;
+    printf("Enter the height of the triangle: ");
+    scanf("%d", &n);
+
+    printTriangle(n);
+
+    return 0;
+}
+/*
+Enter the height of the triangle: 4
+*
+**
+***
+****
+*/
